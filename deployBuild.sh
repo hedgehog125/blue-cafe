cb499bf30078e5b8f65fc52080038ed11ffc0d8a
Hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-08-07 00:44:23 +0100

    I wonder if this script will work

---
+++ deployBuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+{ # Because this file is deleted on branch switch 
+    cd $(dirname $0)
+    if [ -n "$(git status --porcelain=v1 2>/dev/null)" ]
+    then
+        echo "Please commit your changes before running"
+        exit
+    fi
+    
+    tmpDir=$(mktemp tmp.XXXXXXXXXX -td)
+    cp -r static/build/* ${tmpDir}
+    git checkout gh-pages
+    
+    rm -r *
+    mv ${tmpDir}/* .
+    
+    rm -r ${tmpDir}
+    
+    echo "
+    Now make a commit for this new build and change back to the main branch.
+    "
+   exit 
+}
